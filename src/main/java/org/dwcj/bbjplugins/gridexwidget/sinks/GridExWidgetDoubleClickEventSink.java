0579fd1d3a98c030c074edd86d8a853b9397da53
Stephan Wald <swald@basis.com>
2023-02-05 11:53:43 +0100

    initial commit - move grid and thread code from engine

---
+++ src/main/java/org/dwcj/bbjplugins/gridexwidget/sinks/GridExWidgetDoubleClickEventSink.java
@@ -0,0 +1,35 @@
+package org.dwcj.bbjplugins.gridexwidget.sinks;
+
+import com.basis.bbj.proxies.sysgui.BBjControl;
+import org.dwcj.Environment;
+import org.dwcj.bbjplugins.gridexwidget.GridExWidget;
+import org.dwcj.bbjplugins.gridexwidget.events.GridExWidgetSelectEvent;
+import org.dwcj.bridge.ControlAccessor;
+
+import java.util.function.Consumer;
+
+public final class GridExWidgetDoubleClickEventSink {
+
+    private final Consumer<GridExWidgetSelectEvent> target;
+    private final GridExWidget grid;
+
+    @SuppressWarnings({"static-access"})
+    public GridExWidgetDoubleClickEventSink(GridExWidget grid, Consumer<GridExWidgetSelectEvent> target) {
+        this.target = target;
+        this.grid = grid;
+
+        BBjControl bbjctrl = null;
+        try {
+            bbjctrl=ControlAccessor.getDefault().getBBjControl(grid);
+            bbjctrl.setCallback(Environment.getInstance().getBBjAPI().ON_GRID_DOUBLE_CLICK, Environment.getInstance().getDwcjHelper().getEventProxy(this, "onEvent", "::BBjGridExWidgetEventProxies.bbj::BBjGridExWidgetDoubleClickEventProxy"), "onEvent");
+        } catch (Exception e) {
+            Environment.logError(e);
+        }
+    }
+
+    public void onEvent(String eventString) {
+        GridExWidgetSelectEvent dwcEv = new GridExWidgetSelectEvent(grid, eventString);
+        target.accept(dwcEv);
+    }
+
+}
